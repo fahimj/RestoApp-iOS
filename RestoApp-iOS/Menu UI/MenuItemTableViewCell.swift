543ecec59d67572aa1a0abe1f1c19019bbc668b9
Fahim <fahim.jatmiko@gmail.com>
2022-03-26 22:18:33 +0700

    added MenuItemTableViewCell

---
+++ RestoApp-iOS/Menu UI/MenuItemTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MenuItemTableViewCell.swift
+//  RestoApp-iOS
+//
+//  Created by Fahim Jatmiko on 26/03/22.
+//
+
+import UIKit
+
+class MenuItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
