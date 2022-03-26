0b8222b8e4a9d91cd0752fb75b4ff6387b59fef7
Fahim <fahim.jatmiko@gmail.com>
2022-03-26 13:47:33 +0700

    hide DTOs access inside their mappers

--- RestoApp-iOS/MenuLoader/DetailDtoMapper.swift
+++ RestoApp-iOS/MenuLoader/DetailDtoMapper.swift
@@ -12,6 +12,84 @@ final class DetailDtoMapper {
         case invalidJson
     }
     
+    private struct DetailRoot : Codable {
+
+        let id : String
+        let addons : [DetailAddon]
+        let descriptionField : String
+        let discountPercent : Int
+        let displayPrice : Double
+        let imageUrl : String
+        let isDiscount : Bool
+        let name : String
+        let price : Double
+        let tags : [String]
+        let variants : [DetailVariant]
+
+        enum CodingKeys: String, CodingKey {
+            case id = "_id"
+            case addons = "addons"
+            case descriptionField = "description"
+            case discountPercent = "discount_percent"
+            case displayPrice = "display_price"
+            case imageUrl = "image_url"
+            case isDiscount = "is_discount"
+            case name = "name"
+            case price = "price"
+            case tags = "tags"
+            case variants = "variants"
+        }
+        
+        var menuItem:MenuItem {
+            let variants = variants.map{Variant(id: $0.id, name: $0.name)}
+            let addonCategories = self.addons.map{dto -> AddonCategory in
+                let addons = dto.addonItems.map{Addon(id: $0.id, name: $0.name, additionalPrice: $0.additionalPrice)}
+                return AddonCategory(id: dto.addonCategoryId, name: dto.addonCateogryName, addons: addons)
+            }
+            return MenuItem(id: id, descriptionField: descriptionField, discountPercent: discountPercent, displayPrice: displayPrice, imageUrl: imageUrl, isDiscount: isDiscount, name: name, price: price, tags: tags, variants: variants, addOnCategories: addonCategories)
+        }
+    }
+
+    private struct DetailVariant : Codable {
+
+        let id : String
+        let name : String
+
+
+        enum CodingKeys: String, CodingKey {
+            case id = "_id"
+            case name = "name"
+        }
+    }
+
+    private struct DetailAddon : Codable {
+
+        let addonCategoryId : String
+        let addonCateogryName : String
+        let addonItems : [DetailAddonItem]
+
+
+        enum CodingKeys: String, CodingKey {
+            case addonCategoryId = "addon_category_id"
+            case addonCateogryName = "addon_cateogry_name"
+            case addonItems = "addon_items"
+        }
+    }
+
+    private struct DetailAddonItem : Codable {
+
+        let id : String
+        let additionalPrice : Double
+        let name : String
+
+        enum CodingKeys: String, CodingKey {
+            case id = "_id"
+            case additionalPrice = "additional_price"
+            case name = "name"
+        }
+    }
+
+    
     private init() {}
     
     static func map(data:Data) throws -> MenuItem {
--- RestoApp-iOS/MenuLoader/HomeDtoMapper.swift
+++ RestoApp-iOS/MenuLoader/HomeDtoMapper.swift
@@ -12,6 +12,72 @@ final class HomeDtoMapper {
         case invalidJson
     }
     
+    private struct HomeRoot : Decodable {
+        let categories : [HomeCategory]
+        var list : [HomeList]
+
+        enum CodingKeys: String, CodingKey {
+            case categories = "categories"
+            case list = "list"
+        }
+        
+        var categoryItems:[Category] {
+            var resultCategories = categories.map{Category(id: $0.id ?? "", name: $0.name ?? "", items: [])}
+            list.forEach({listItem in
+                let menuItems = listItem.items.map{MenuItem(id: $0.id, descriptionField: $0.descriptionField, discountPercent: $0.discountPercent, displayPrice: $0.displayPrice, imageUrl: $0.imageUrl, isDiscount: $0.isDiscount, name: $0.name, price: $0.price, tags: $0.tags, variants: [], addOnCategories: [])}
+                guard let categoryIndex = resultCategories.firstIndex(where: {$0.id == listItem.categoryId}) else {return}
+                resultCategories[categoryIndex].items = menuItems
+            })
+            
+            return resultCategories
+        }
+    }
+
+    private struct HomeList : Decodable {
+        let categoryId : String
+        let items : [HomeItem]
+
+        enum CodingKeys: String, CodingKey {
+            case categoryId = "category_id"
+            case items = "items"
+        }
+    }
+
+    private struct HomeItem : Decodable {
+        let id : String
+        let descriptionField : String
+        let discountPercent : Int
+        let displayPrice : Double
+        let imageUrl : String
+        let isDiscount : Bool
+        let name : String
+        let price : Double
+        let tags : [String]
+
+        enum CodingKeys: String, CodingKey {
+            case id = "_id"
+            case descriptionField = "description"
+            case discountPercent = "discount_percent"
+            case displayPrice = "display_price"
+            case imageUrl = "image_url"
+            case isDiscount = "is_discount"
+            case name = "name"
+            case price = "price"
+            case tags = "tags"
+        }
+    }
+
+    private struct HomeCategory : Decodable {
+        let id : String?
+        let name : String?
+
+        enum CodingKeys: String, CodingKey {
+            case id = "_id"
+            case name = "name"
+        }
+    }
+
+    
     private init() {}
     
     static func map(data:Data) throws -> [Category] {
